49bd0c68f65ffeafc893ba9919e498173dae251d
itschurry <cheolhee@icloud.com>
2026-01-15 16:30:43 +0900

    BarcodeReaderNode: Add parameter for QR image topic and update publisher

--- zbar_ros/src/barcode_reader_node.cpp
+++ zbar_ros/src/barcode_reader_node.cpp
@@ -75,6 +75,7 @@ BarcodeReaderNode::BarcodeReaderNode() : Node("barcode_reader_node") {
 
     image_topic_ = this->declare_parameter<std::string>("image_topic", "camera/image/compressed");
     qr_code_topic_ = this->declare_parameter<std::string>("qr_code_topic", "/barcode/code_string");
+    qr_image_topic_ = this->declare_parameter<std::string>("qr_image_topic", "/barcode/qr_image");
     RCLCPP_DEBUG(get_logger(), "Subscribing to topics: %s, %s", image_topic_.c_str(), qr_code_topic_.c_str());
 
     camera_sub_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
@@ -82,7 +83,7 @@ BarcodeReaderNode::BarcodeReaderNode() : Node("barcode_reader_node") {
 
     symbol_pub_ = this->create_publisher<zbar_ros_interfaces::msg::Symbol>("symbol", 10);
     barcode_pub_ = this->create_publisher<std_msgs::msg::String>(qr_code_topic_, 10);
-    barcode_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/barcode/image", 10);
+    barcode_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>(qr_image_topic_, 10);
 
     throttle_ = this->declare_parameter<double>("throttle_repeated_barcodes", 0.0);
     RCLCPP_DEBUG(get_logger(), "throttle_repeated_barcodes : %f", throttle_);
